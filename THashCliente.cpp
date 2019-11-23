29845f57e7ddeaf080fe0eef1e49d5b17a5faf2a
Miguel González García <mgg00112@red.ujaen.es>
2019-11-23 16:47:53 +0100

    Funciones hash nuevas

--- THashCliente.cpp
+++ THashCliente.cpp
@@ -42,7 +42,7 @@ bool THashCliente::inserta(const std::string& dni, Cliente &cli){
     unsigned long clave=djb2((unsigned char*)dni.c_str());
 
     while (!encontrado) {
-        y=fcuadratica(clave, i);           
+        y=hash(clave, i);           
         if (v[y].marca==vacia ) {
             if (p==-1)
                 final=y;
@@ -51,9 +51,9 @@ bool THashCliente::inserta(const std::string& dni, Cliente &cli){
             taml++;
             v[final].dni=dni;
             v[final].marca=ocupada;
-            v[final].clave=clave;                             
-            v[final].dato=cli;  //push_back(dato);                                                
-            encontrado = true;   //Encontre un sitio libre  
+            v[final].clave=clave;
+            v[final].dato=cli;
+            encontrado = true;
         }else{
             if (v[y].marca==disponible && aux==false){
                 p=y;
@@ -78,7 +78,7 @@ bool THashCliente::borra(long int clave){
     unsigned x,i=0;
     bool borrado = false;
     while (!borrado) {
-        x=fdoble(clave, i);
+        x=hash2(clave, i);
         //si no esta ocupada por su termino, mira si esta libre para dejar de buscar
         if (v[x].marca==ocupada){
                 v[x].marca=disponible; //lo borro y la dejo marcada
@@ -100,7 +100,7 @@ bool THashCliente::busca(const std::string& dni, Cliente* &cli){
     bool esta = false;
     unsigned long clave=djb2((unsigned char*)dni.c_str());
     while (!esta) {
-        x=fdoble(clave, i);
+        x=hash2(clave, i);
         if (v[x].marca==ocupada){                    
             return &(v[x].dato);       
         }else{
